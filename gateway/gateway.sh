9d2af437d0a091e08ef8a8d8223a645d49927b53
Elias Datler <elias.datler@dfinity.org>
2024-07-03 14:31:27 +0000

    Update ports for gateway

--- gateway/gateway.sh
+++ gateway/gateway.sh
@@ -9,18 +9,18 @@ set -e
 
 REPLICA_URL="${1}"
 
-# PocketIC server: bind the server at port 8000
+# PocketIC server: bind the server at port 7000
 
 rm -f pocket-ic.port
-pocket-ic --port 8000 --ttl 2592000 --port-file pocket-ic.port &
+pocket-ic --port 7000 --ttl 2592000 --port-file pocket-ic.port &
 while [ ! -f pocket-ic.port ]
 do
   sleep 1
 done
 
-# Start an HTTP gateway at port 8080
+# Start an HTTP gateway at port 8000
 
-curl -X POST -H "Content-Type: application/json" http://localhost:8000/http_gateway -d "{\"listen_at\": 8080, \"forward_to\": {\"Replica\": \"${REPLICA_URL}\"}, \"domain\": \"localhost\"}"
+curl -X POST -H "Content-Type: application/json" http://localhost:7000/http_gateway -d '{"listen_at": 8000, "forward_to": {"Replica": '${REPLICA_URL}'}, "domain": "localhost"}'
 
 # Block until the HTTP gateway container is killed
 
